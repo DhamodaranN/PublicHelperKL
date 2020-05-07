59f6c49e39eef0619bb660dd743b48c4c1312965
rsreshma <rsreshma042@gmail.com>
2020-05-07 19:47:25 +0530

    user dashboard added

--- app/src/main/java/in/gov/mhrd/helper/UserDashboard.java
+++ app/src/main/java/in/gov/mhrd/helper/UserDashboard.java
@@ -10,5 +10,6 @@ public class UserDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+        System.out.println("hello");
     }
 }
