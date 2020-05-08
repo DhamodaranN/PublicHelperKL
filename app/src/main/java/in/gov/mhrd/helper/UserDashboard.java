7d05d0db8b619ad3eeb3dc3d23fd4a0fd3b578a5
rsreshma <rsreshma042@gmail.com>
2020-05-08 22:20:49 +0530

    user dashboard added

--- app/src/main/java/in/gov/mhrd/helper/UserDashboard.java
+++ app/src/main/java/in/gov/mhrd/helper/UserDashboard.java
@@ -2,7 +2,10 @@ package in.gov.mhrd.helper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserDashboard extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class UserDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
         System.out.println("hello");
+    }
+
+    public void HealthWorkerreg(View view) {
+        Intent i=new Intent(this, Register.class);
+      i.putExtra("type","health");
+      startActivity(i);
+
     }
 }
