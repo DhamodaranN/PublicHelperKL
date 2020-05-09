b535db2b47ab5456a22a3ae93164a89f23be1023
samuelshan123 <samuelshan123@gmail.com>
2020-05-09 10:42:08 -0700

    first commit with layouts

--- app/src/main/java/in/gov/mhrd/helper/Tollfree.java
+++ app/src/main/java/in/gov/mhrd/helper/Tollfree.java
@@ -6,41 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tollfree extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tollfree);
-
-
-        Spinner spinnerdist = (Spinner) findViewById(R.id.district);
-
-        // Spinner Drop down elements
-        List<String> dist = new ArrayList<String>();
-        dist.add("Select(dist)");
-        dist.add("kanya kumari");
-        dist.add("madurai");
-        dist.add("theni");
-
-
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapterdist = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dist);
-
-        // Drop down layout style - list view with radio button
-
-        dataAdapterdist.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinnerdist.setAdapter(dataAdapterdist);
     }
-
 
 }
